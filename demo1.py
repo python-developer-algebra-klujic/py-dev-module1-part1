ad074cc1393b93747e62db060ff0905f064c5d78
python-developer-algebra-klujic <DELETED-1700289874152>
2023-11-21 19:46:05 +0000

    added demo files

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print('Dobro dosli u PyDev program obrazovanja!')
+
+first_name = 'Pero'
+last_name = 'Peric'
+date_of_birth = 1998 # 1.998,00
+mobile_phone = '+385 9n 1234 567'
+employed = True
+height = 180.99
+
+print(first_name, last_name)
