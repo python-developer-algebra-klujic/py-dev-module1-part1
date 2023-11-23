206e333d6fab8604b5250dd223b6f032e75bc3c3
python-developer-algebra-klujic <DELETED-1700289874152>
2023-11-23 20:13:57 +0000

    Added print demo

---
+++ print_demo.py
@@ -0,0 +1,13 @@
+first_name = 'Pero'
+last_name  = 'Peric'
+phone = '+385 9_ 1234 567'
+end_value = ' '
+
+# print()
+# print(first_name, end=' ')
+# print(first_name, last_name, phone)
+# #print(first_name, last_name, phone, sep=' ')
+
+# print(first_name, last_name, phone, sep=', ')
+print(first_name, end=end_value)
+print(last_name, phone, sep=', ', end=end_value)
